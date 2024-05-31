8f87aa817c0ae85c4726b2d907913695d4059dc5
AnvaR <lord_1112@mail.ru>
2024-06-01 00:38:02 +0300

    fixing errors in the file engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,7 +3,7 @@ import prompt
 
 def game_engine(game):
     print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.DESCRIPTION)
 
@@ -16,7 +16,7 @@ def game_engine(game):
         if user_answer == correct_answer:
             print('Correct!')
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")  # NOQA
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
 
