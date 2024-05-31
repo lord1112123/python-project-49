fed5c9d2ec699bf537d7fdb2914ac10a534a95dd
AnvaR <lord_1112@mail.ru>
2024-06-01 00:18:48 +0300

    fixing errors in the file calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,28 +1,12 @@
 import random
 
+DESCRIPTION = 'What is the result of the expression?'
 
-def main():
-    print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('What is the result of the expression?')
+def generate_game():
+    random_number1 = int(random.randint(1, 100))
+    random_number2 = int(random.randint(1, 100))
+    operators = random.choice(['+', '-', '*'])
+    question = f'{ran_num1} {operators} {ran_num2}'
+    correct_answer = str(eval(question))
 
-    for _ in range(3):
-        ran_num1 = int(random.randint(1, 100))
-        ran_num2 = int(random.randint(1, 100))
-        operators = random.choice(['+', '-', '*'])
-        question = f'{ran_num1} {operators} {ran_num2}'
-        corr_ans = str(eval(question))
-        print(f'Question: {question}')
-        user_ans = input('Your answer: ')
-        if user_ans.lower() == corr_ans:
-            print('Correct!')
-        else:
-            print(f"'{user_ans}' is wrong answer ;(. Correct answer was '{corr_ans}'.")
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f'Congratulations, {name}!')
-
-if __name__ == "__main__":
-    main()
+    return question, correct_answer
