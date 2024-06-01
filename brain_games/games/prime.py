5f1fb2b8bd270ef9e69db49e2cf0abbb8e6c68b6
AnvaR <lord_1112@mail.ru>
2024-06-01 23:53:38 +0300

    modified prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -14,28 +14,8 @@ def is_prime(number):
     return True
 
 
-def main():
-    print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    
-    for _ in range(3):
-        random_number = random.randint(1, 100)
-        corr_ans = 'yes' if is_prime(random_number) else 'no'
-
-        print(f'Question: {random_number}')
-        user_ans = input('Your answer: ')
-
-        if user_ans.lower() == corr_ans:
-            print('Correct!')
-        else:
-            print(f"'{user_ans}' is wrong answer ;(. Correct answer was '{corr_ans}'.")
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f'Congratulations, {name}!')
-
-
-if __name__ == "__main__":
-    main()
+def generate_game():
+    random_number = random.randint(1, 100)
+    question = random_number
+    correct_answer = 'yes' if is_prime(random_number) else 'no'
+    return question, correct_answer
