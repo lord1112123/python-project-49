4b2383c08c339c8408b38d6f59f84209aa795e75
AnvaR <lord_1112@mail.ru>
2024-06-02 00:59:29 +0300

    modified brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,42 +1,9 @@
-import random
-
-
-def arif_progress(a, d, k):
-    progress = [a + i * d for i in range(k)]
-    return progress
-
-
-def hide_element(progress, index):
-    hide_value = progress[index]
-    progress_hide = progress[:]
-    progress_hide[index] = ".."
-    return progress_hide, hide_value
+from brain_games import engine
+from brain_games.games import progression
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('What number is missing in the progression?')
-
-    for _ in range(3):
-        ran_num1 = int(random.randint(1, 10))
-        ran_num2 = int(random.randint(1, 10))
-        ran_num3 = int(random.randint(5, 10))
-        hide_index = random.randint(0, ran_num3 - 1)
-        progress = arif_progress(ran_num1, ran_num2, ran_num3)
-        question, corr_ans = hide_element(progress, hide_index)
-        question_str = ' '.join(map(str, question))
-        print(f'Question: {question_str}')
-        user_ans = int(input('Your answer: '))
-        if user_ans == corr_ans:
-            print('Correct!')
-        else:
-            print(f"'{user_ans}' is wrong answer ;(. Correct answer was '{corr_ans}'.")
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f'Congratulations, {name}!')
+    engine.game_engine(progression)
 
 
 if __name__ == "__main__":
