34265a0317fa84a47e766e3118a86ac4aa4438b7
AnvaR <lord_1112@mail.ru>
2024-06-01 23:04:35 +0300

    fixed calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,7 +6,7 @@ def generate_game():
     random_number1 = int(random.randint(1, 100))
     random_number2 = int(random.randint(1, 100))
     operators = random.choice(['+', '-', '*'])
-    question = f'{ran_num1} {operators} {ran_num2}'
+    question = f'{random_number1} {operators} {random_number2}'
     correct_answer = str(eval(question))
 
     return question, correct_answer
