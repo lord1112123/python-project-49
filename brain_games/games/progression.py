ce720a6c6797fadae89b2807339d901530dc23cb
AnvaR <lord_1112@mail.ru>
2024-06-02 00:13:43 +0300

    fixing progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,5 +1,6 @@
 import random
 
+DESCRIPTION = 'What number is missing in the progression?'
 
 def arif_progress(a, d, k):
     progress = [a + i * d for i in range(k)]
@@ -13,31 +14,11 @@ def hide_element(progress, index):
     return progress_hide, hide_value
 
 
-def main():
-    print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('What number is missing in the progression?')
-
-    for _ in range(3):
-        ran_num1 = int(random.randint(1, 10))
-        ran_num2 = int(random.randint(1, 10))
-        ran_num3 = int(random.randint(5, 10))
-        hide_index = random.randint(0, ran_num3 - 1)
-        progress = arif_progress(ran_num1, ran_num2, ran_num3)
-        question, corr_ans = hide_element(progress, hide_index)
-        question_str = ' '.join(map(str, question))
-        print(f'Question: {question_str}')
-        user_ans = int(input('Your answer: '))
-        if user_ans == corr_ans:
-            print('Correct!')
-        else:
-            print(f"'{user_ans}' is wrong answer ;(. Correct answer was '{corr_ans}'.")
-            print(f"Let's try again, {name}!")
-            return
-    
-    print(f'Congratulations, {name}!')
-
-
-if __name__ == "__main__":
-    main()
+def generate_game():
+    number1 = int(random.randint(1, 10))
+    number2 = int(random.randint(1, 10))
+    number3 = int(random.randint(5, 10))
+    hide_index = random.randint(0, number3 - 1)
+    progress = arif_progress(number1, number2, number3)
+    question, correct_answer = hide_element(progress, hide_index)
+    question_str = ' '.join(map(str, question))
