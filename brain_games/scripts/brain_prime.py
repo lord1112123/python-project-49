343eb50cc36277e9bb1d09f7d1fc5a28d8b665df
AnvaR <lord_1112@mail.ru>
2024-06-02 00:02:18 +0300

    modified brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,40 +1,9 @@
-import math
-import random
-
-
-def is_prime(number):
-    if number <= 1:
-        return False
-    number_sqrt = int(math.sqrt(number))
-    divisors = range(2, (number_sqrt + 1))
-
-    for element in divisors:
-        if number % element == 0:
-            return False
-    return True
+from brain_games import engine
+from brain_games.games import prime
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = input('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-
-    for _ in range(3):
-        random_number = random.randint(1, 100)
-        corr_ans = 'yes' if is_prime(random_number) else 'no'
-
-        print(f'Question: {random_number}')
-        user_ans = input('Your answer: ')
-
-        if user_ans.lower() == corr_ans:
-            print('Correct!')
-        else:
-            print(f"'{user_ans}' is wrong answer ;(. Correct answer was '{corr_ans}'.")
-            print(f"Let's try again, {name}!")
-            return
-
-    print(f'Congratulations, {name}!')
+    engine.game_engine(prime)
 
 
 if __name__ == "__main__":
