df1a0dffad1b25a80f2eefe1c8608c196091a488
AnvaR <lord_1112@mail.ru>
2024-06-03 23:48:33 +0300

    correction gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -12,6 +12,6 @@ def gcd(a, b):
 def generate_game():
     random_number1 = int(random.randint(1, 100))
     random_number2 = int(random.randint(1, 100))
-    question = f'{random_number1}, {random_number2}'
+    question = f'{random_number1} {random_number2}'
     correct_answer = str(gcd(random_number1, random_number2))
     return question, correct_answer
