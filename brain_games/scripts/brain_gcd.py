11620546213859bfaf226483c74956357575e2c0
AnvaR <lord_1112@mail.ru>
2024-06-06 13:01:19 +0300

    changing the name of the game engine

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,9 @@
-from brain_games import engine
+from brain_games import run_game
 from brain_games.games import gcd
 
 
 def main():
-    engine.game_engine(gcd)
+    run_game.game_engine(gcd)
 
 
 if __name__ == "__main__":
