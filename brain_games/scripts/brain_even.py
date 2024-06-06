158bdafce4ed83c80d54ec1ec2e57bc276bf2019
AnvaR <lord_1112@mail.ru>
2024-06-06 13:00:46 +0300

    changing the name of the game engine

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,9 @@
-from brain_games import engine
+from brain_games import run_game
 from brain_games.games import even
 
 
 def main():
-    engine.game_engine(even)
+    run_game.game_engine(even)
 
 
 if __name__ == "__main__":
