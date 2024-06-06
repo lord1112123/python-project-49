db0d9b8f6ab4691ee76845ea0494ddb90bc1b440
AnvaR <lord_1112@mail.ru>
2024-06-06 12:59:46 +0300

    changing the name of the game engine

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,9 @@
-from brain_games import engine
+from brain_games import run_game
 from brain_games.games import calc
 
 
 def main():
-    engine.game_engine(calc)
+    run_game.game_engine(calc)
 
 
 if __name__ == "__main__":
